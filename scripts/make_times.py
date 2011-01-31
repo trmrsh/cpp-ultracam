feaf589c63c3d2121d026858ae8f91b2a1a3078c
Tom Marsh <t.r.marsh@warwick.ac.uk>
2011-01-31 11:48:52 +0000

    Changes for new data structures
    Slight reorganisation of directory structures required a few changes to scripts such as this one.

--- scripts/make_times.py
+++ scripts/make_times.py
@@ -1,26 +1,19 @@
 #!/usr/bin/env python
 
 """
-Script to generate timing files for ULTRACAM and ULTRASPEC. 
+Script to generate timing files for ULTRACAM and ULTRASPEC. Run this in the 
+'logs' directory
 
 It expects directories of the form '2005-11' (Nov 2005) which have
 a structure like so:
 
 2005-11
-  2005-11-23
-    data             -- directory with run###.xml and run###.dat files    
-  2005-11-24  -- directory for 24 Nov
-    data      -- directory with run###.xml and run###.dat files    
-
-etc. It then winds through all of the data and generates
-files such as 2005-11-23.times which will be placed in the 2005-11-23
-directory. It will only do this if there is no such file already.
+  2005-11-23 -- directory with data subdirectory containing run###.xml and run###.dat files.
+                File called 2005-11-23.times will be created here if not already present.
 
 """
 
-import os
-import sys
-import re
+import os, sys, re
 import trm.subs as subs
 import subprocess as subproc
 
@@ -31,57 +24,57 @@ if __name__ == "__main__":
     # The main program
 
     # Create a list directories of runs to search through
-    rdirs = [x for x in os.listdir(os.curdir) if os.path.isdir(x) and x.startswith('20')]
+    dre1 = re.compile(r'^\d\d\d\d-\d\d$')
+    dre2 = re.compile(r'^\d\d\d\d-\d\d-\d\d$')
+
+    rdirs = [x for x in os.listdir(os.curdir) if os.path.isdir(x) and dre1.match(x)]
+
     for rdir in rdirs:
 
-        # Now the night-by-night directories
-        ndirs = [x for x in os.listdir(rdir) if os.path.isdir(os.path.join(rdir,x)) and x.startswith('20')]
+        # Now the night-by-night directories, must be in both raw and log directories
+        ndirs = [x for x in os.listdir(rdir) if dre2.match(x) and os.path.isdir(os.path.join(rdir,x))]
 
         for ndir in ndirs:
 
-            npath = os.path.join(rdir, ndir)
+            dpath = os.path.join(rdir, ndir, 'data')
+            times = os.path.join(rdir, ndir, ndir + '.times')
 
-            times = os.path.join(npath, ndir + '.times')
-
-
+            # only try to construct times file if it does not exist
             if not os.path.exists(times):
-                data  = os.path.join(npath, 'data')
-                if not os.path.isdir(data):
-                    print 'File = ' + times + ' does not exist, but ' + data + ' does not either.'
-                else:                    
-                    ft = open(times,'w')
-                    dfiles = [os.path.join(data, x[:-4]) for x in os.listdir(data) if x.startswith('run') and x.endswith('.xml')]
+                print 'File =',times,'does not exist; will create.'
+                ft = open(times,'w')
+                dfiles = [os.path.join(dpath, x[:-4]) for x in os.listdir(dpath) if x.startswith('run') and x.endswith('.xml')]
 
-                    for dfile in dfiles:
-                        args = (gettime, dfile)
-                        output  = subproc.Popen(args, stdout=subproc.PIPE, stderr=subproc.PIPE).communicate()[0].split('\n')
-                        run     = 'UNDEF'
-                        date    = 'UNDEF'
-                        utstart = 'UNDEF'
-                        utend   = 'UNDEF'
-                        ngood   = 'UNDEF'
-                        nframe  = 'UNDEF'
-                        expose  = 'UNDEF'
-                        sample  = 'UNDEF'
-                        for line in output:
-                            arr = line.split()
-                            if line.startswith('Run'):
-                                run = arr[2][-6:]
-                            elif line.startswith('Number of frames'):
-                                nframe = arr[4]
-                            elif line.startswith('Number of good frames'):
-                                ngood = arr[5]
-                            elif line.startswith('OK run length'):
-                                expose = arr[4]
-                                sample = arr[9]
-                            elif line.startswith('UT at start'):
-                                date    = arr[4][:-1]
-                                utstart = arr[5]
-                            elif line.startswith('UT at end'):
-                                utend   = arr[5]
-                        ft.write(run + ' ' + date + ' ' + utstart + ' ' + utend + ' ' + ngood + ' ' + expose + ' ' + sample + '\n')
-                    ft.close()
-                    print 'Created file =',times
+                for dfile in dfiles:
+                    args = (gettime, dfile)
+                    output  = subproc.Popen(args, stdout=subproc.PIPE, stderr=subproc.PIPE).communicate()[0].split('\n')
+                    run     = 'UNDEF'
+                    date    = 'UNDEF'
+                    utstart = 'UNDEF'
+                    utend   = 'UNDEF'
+                    ngood   = 'UNDEF'
+                    nframe  = 'UNDEF'
+                    expose  = 'UNDEF'
+                    sample  = 'UNDEF'
+                    for line in output:
+                        arr = line.split()
+                        if line.startswith('Run'):
+                            run = arr[2][-6:]
+                        elif line.startswith('Number of frames'):
+                            nframe = arr[4]
+                        elif line.startswith('Number of good frames'):
+                            ngood = arr[5]
+                        elif line.startswith('OK run length'):
+                            expose = arr[4]
+                            sample = arr[9]
+                        elif line.startswith('UT at start'):
+                            date    = arr[4][:-1]
+                            utstart = arr[5]
+                        elif line.startswith('UT at end'):
+                            utend   = arr[5]
+                    ft.write(run + ' ' + date + ' ' + utstart + ' ' + utend + ' ' + ngood + ' ' + expose + ' ' + sample + '\n')
+                ft.close()
+                print 'Created file =',times
                             
 
 
