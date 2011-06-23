d9d60ae5a6110a58ec62b158d8f1984c0bf58a95
Tom Marsh <t.r.marsh@warwick.ac.uk>
2011-06-23 09:47:02 +0100

    Modified the operation of this script to do the copying.
    This now invokes scp to carry out the copying prior to deletion.

--- scripts/clear_files.py
+++ scripts/clear_files.py
@@ -1,35 +1,27 @@
 #!/usr/bin/env python
 
 """
-Script to clear out files from the ultracam archive.
+Script to clear out junk files from the ultracam archive.
 Any run with a corresponding '.jnk' file (which can be empty, 
 so just touching such a file is enough to flag it) counts as 
 junk. If such a file is found with an equivalent .xml and .dat, 
-the .dat will be moved to a different location; the xml will be 
-copied along with it. Similarly any file with a corresponding
-'.mov' file will be moved (with any luck the .mov file will contain 
-a reason for doing so).
+the .dat and .xml will be copied to astronas while the .dat
+file will be deleted.
 
-Run on scafell. The files are copied to the same directory tree 
-under /tmp on scafell but you should take a copy as these will be 
-wiped should there ever be a re-install. The following commands
-will do this:
-
-cd /tmp
-rsync -av ultracam astronas: 
+This should be run from the top-level ultracam directory on storage.
 """
 
-import os
+import os, sys, re, shutil, subprocess
 from os.path import join, isfile, exists, isdir, getsize
-import sys
-import re
-import shutil
+
+cwd = os.getcwd()
+if cwd != '/storage/astro2/phsaap/ultracam':
+    print 'This must be run from /storage/astro2/phsaap/ultracam'
+    exit(1)
 
 # File matching regular expressions
-ddir_re = re.compile('\d\d\d\d-\d\d/\d\d\d\d-\d\d-\d\d/data$') # Search for YYYY-MM/YYYY-MM-DD/data
+ddir_re = re.compile('raw_data/\d\d\d\d-\d\d-\d\d$') # Search for data directories
 xml_re  = re.compile('run\d\d\d\.xml$') # Search for xml files
-
-save_dir = '/tmp/ultracam'
 
 # Walk through whole tree, selecting only data directories
 
@@ -38,44 +30,47 @@ for root, dirs, files in os.walk('.'):
     if ddir_re.search(root):
 
         # create a list of files to be moved/copied
-        runs = [x[:-4] for x in files if isfile(join(root, x)) and \
-                    (isfile(join(root, x[:-4] + '.jnk')) or isfile(join(root, x[:-4] + '.mov'))) \
-                    and xml_re.match(x) is not None]
-
+        runs = [x[:-4] for x in files if isfile(join(root, x)) and isfile(join(root, x[:-4] + '.jnk')) and xml_re.match(x) is not None]
         for run in runs:
 
-            olddat = join(root, run + '.dat')
-            oldxml = join(root, run + '.xml')
+            # create parent directory on astronas
+            pdir = root[11:]
+            if subprocess.Popen(['ssh', 'astronas', 'cd ultracam/raw_data; mkdir -p %s' % (pdir,)]).wait(): 
+                print 'ERROR: failed to create containing directory',pdir,'on astronas'
+                exit(1)
 
-            if olddat or oldxml:
-
-                # make a directory if need be
-                new_dir = join(save_dir, root[-15:-5])
-                if exists(new_dir) and not isdir(new_dir):
-                    print 'FATAL ERROR: an entity called',new_dir,'exists but is not a directory.'
-                    print 'Please fix this.'
-                    print 'Transferred a total of',int(10.*float(total)/1024**2+0.5)/10.,'MB'
+            data = os.path.join(root, run + '.dat')
+            if os.path.exists(data):
+                if subprocess.Popen(['scp', data, 'astronas:ultracam/' + data]).wait(): 
+                    print 'ERROR: failed to copy',data,'to astronas'
                     exit(1)
-                elif not exists(new_dir):
-                    os.makedirs(new_dir)
-                    pass
-
-                # move the data if possible
-                newdat = join(new_dir, run + '.dat')
-                if not exists(newdat):
-                    shutil.move(olddat, newdat)
-                    total += getsize(newdat)
-                    print 'Moved ' + olddat + ' to ' + newdat
                 else:
-                    print 'File = ',newdat,'already exists and will not be over-written.'
-                    print 'File = ',olddat,'will be left untouched.'
+                    total += os.path.getsize(data)
+                    os.unlink(data)
+                    print 'copied and removed',data
 
-                # copy the xml if possible
-                newxml = join(new_dir, run + '.xml')
-                if not exists(newxml):
-                    shutil.copy(oldxml, newxml)
-                    print 'Copied ' + oldxml + ' to ' + newxml
-                else:
-                    print 'File = ',newxml,'already exists and will not be over-written.'
+                    xml  = os.path.join(root, run + '.xml')
+                    if os.path.exists(xml) and subprocess.Popen(['scp', xml, 'astronas:ultracam/' + xml]).wait(): 
+                        print 'ERROR: failed to copy',xml,'to astronas'
+                        exit(1)
+                    else:
+                        total += os.path.getsize(xml)
+            else:
+                data = os.path.join(root, run + '.dat.gz')
+                if os.path.exists(data):
+                    if subprocess.Popen(['scp', data, 'astronas:ultracam/' + data]).wait(): 
+                        print 'ERROR: failed to copy',data,'to astronas'
+                        exit(1)
+                    else:
+                        total += os.path.getsize(data)
+                        os.unlink(data)
+                        print 'copied and removed',data
+
+                        xml  = os.path.join(root, run + '.xml')
+                        if os.path.exists(xml) and subprocess.Popen(['scp', xml, 'astronas:ultracam/' + xml]).wait(): 
+                            print 'ERROR: failed to copy',xml,'to astronas'
+                            exit(1)
+                        else:
+                            total += os.path.getsize(xml)
 
 print 'Transferred a total of',int(10.*float(total)/1024**2+0.5)/10.,'MB'
